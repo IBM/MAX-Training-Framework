948ce5dea119e386cc8bf22f49dcf6634971a46b
Patrick Titzler <ptitzler@us.ibm.com>
2020-04-27 14:57:21 -0700

    Update supported WML regions

--- max_training_framework/utils/wml.py
+++ max_training_framework/utils/wml.py
@@ -102,7 +102,7 @@ class WMLWrapper:
         # the deep learning functionality we want to use
         # Current list (as of 01/2020): 'us-south' and 'eu-gb'
         h = urlparse(url).hostname
-        if len([sh for sh in ['us-south', 'eu-gb'] if h.startswith(sh)]) == 0:
+        if len([sh for sh in ['us-south', 'eu-de'] if h.startswith(sh)]) == 0:
             raise WMLWrapperError(
                 'Error. WML service location "{}" does not '
                 'provide the required training functionality.'
